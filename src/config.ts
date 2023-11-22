130916d4fccef23aea84e7f1e577ee3071d5df5c
Chris Leveille <cdleveille@gmail.com>
2023-11-22 13:44:55 -0500

    default API_BASE_URL to localhost:5000 if not specified

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,11 @@
 import type { IConfig } from "./types";
 
 export const Config = {
-	API_BASE_URL: Bun.env.API_BASE_URL || undefined,
+	API_BASE_URL: Bun.env.API_BASE_URL || "http://localhost:5000",
 	API_KEY: Bun.env.API_KEY || undefined,
 	REFRESH_DISCS_START: Bun.env.REFRESH_DISCS_START?.toLowerCase() === "true",
 	REFRESH_DISCS_CRON: Bun.env.REFRESH_DISCS_CRON?.toLowerCase() === "true"
 } as IConfig;
 
-if (!Config.API_BASE_URL) throw new Error("API_BASE_URL is not defined!");
-
-if (!Config.API_KEY) throw new Error("API_KEY is not defined!");
+if (!Config.API_KEY)
+	throw new Error("API_KEY is not defined! Must be set equal to the API_KEY in the discit-api project.");
